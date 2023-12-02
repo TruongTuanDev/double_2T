39d8e7a53ea6ae00796fd5ad62583cfd16f7a048
TruongTuanDev <tuanhulonhat@gmail.com>
2023-12-02 12:41:34 +0700

    tuancommit

--- database/migrations/2023_11_16_025849_create_employers_table.php
+++ database/migrations/2023_11_16_025849_create_employers_table.php
@@ -14,17 +14,15 @@ return new class extends Migration
         Schema::create('employers', function (Blueprint $table) {
             $table->id('id_emp');
             $table->string('name_compn',70)->nullable();
-            $table->string('password',50)->nullable();
-            $table->string('phonenumber',20)->nullable();
             $table->text('description')->nullable();
             $table->string('logo',50)->nullable();
             $table->string('background',50)->nullable();
             $table->string('slodan',100)->nullable();
-            $table->string('email',50)->unique();
             $table->text('treatment')->nullable();
             $table->string('website',50)->nullable();
             $table->string('address',100)->nullable();
             $table->integer('scale')->nullable(); 
+            $table->integer('id_user')->nullable(); 
             $table->timestamps();
             $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
---
+++ database/migrations/2023_12_01_140230_create_students_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id('id_stu');
+            $table->string('university');
+            $table->string('major');
+            $table->string('birthday');
+            $table->string('avatar');
+            $table->string('description');
+            $table->integer('id_user');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
