7e50543ab411d7f24ea122c074dd48b4590e280c
Nguyen Thi Tam <tamn7161@gmail.com>
2023-12-10 10:38:33 +0700

    css job-detail

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -13,7 +13,7 @@ use App\Repositories\Interfaces\UserRepositoryInterface;
 class UserRepository extends BaseRepository implements UserRepositoryInterface 
 {
   protected $model;
-  public function __construct(Admin $model){
+  public function __construct(User $model){
     $this->model = $model;
   }
   public function getAllPaginate($number){
